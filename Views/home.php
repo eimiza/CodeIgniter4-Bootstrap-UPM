e68ed7928ae9c3b0047529da3613fe29425352ed
Eimiza Faisha Azmi <eimiza@ict.upsi.edu.my>
2022-10-11 11:59:49 +0800

    Create home.php

---
+++ Views/home.php
@@ -0,0 +1,8 @@
+<div class="row">
+  <div class="col-md-12">
+    This is a default home location <br><br>
+    Controller location: /app/Controllers/Home.php<br>
+    View location: /app/Views/home.php<br>
+
+  </div>
+</div>
